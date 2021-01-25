8fcd31fbe780516d7633fafcda43be3565f06651
Jumpshawt <heplaysvidogames@gmail.com>
2021-01-25 13:13:34 -0800

    multiplaeyr
    multiple players work now

--- Game.gd
+++ Game.gd
@@ -7,27 +7,77 @@ onready var player2pos = $Player2Pos
 # var a = 2
 # var b = "text"
 
+const MAX_PLAYERS : int = 32 
+var ip_adress = "127.0.0.1"
 
 # Called when the node entsers the scene tree for the first time.
 func _ready():
+
+
+
+#func _process(delta):
+#	pass
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _on_Button_Host_pressed():
+	ip_adress = $Lobby/ip.text
+	var port = int($Lobby/port.text)
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(port, MAX_PLAYERS)
+	get_tree().set_network_peer(peer)
+	print("hosting")
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 	var player1 = preload("res://Player.tscn").instance()
 	player1.set_name(str(get_tree().get_network_unique_id()))
 	player1.set_network_master(get_tree().get_network_unique_id())
 	player1.global_transform = player1pos.global_transform
 	Globals.respawn1 = player1pos.global_transform
-	
+	print("adding player 1 at" , player1pos.global_transform)
+	add_child(player1)
+	menu_vis()
+	pass # Replace with function body.
+
+
+func _on_Button_join_pressed():
+	ip_adress = $Lobby/ip.text
+	var port = int($Lobby/port.text)
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(ip_adress, port)
+	get_tree().set_network_peer(peer)
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("peer_disconnected", self, "_player_disconnected")
+	var player1 = preload("res://Player.tscn").instance()
+	player1.set_name(str(get_tree().get_network_unique_id()))
+	player1.set_network_master(get_tree().get_network_unique_id())
+	player1.global_transform = player1pos.global_transform
+	Globals.respawn1 = player1pos.global_transform
+	print("adding player 1 at" , player1pos.global_transform)
+	add_child(player1)
+	pass # Replace with function body.
+
+func _player_connected(id):
+	print("epic")
+	Globals.player2id = id
 	var player2 = preload("res://Player.tscn").instance()
 	player2.set_name(str(Globals.player2id))
 	player2.set_network_master(Globals.player2id)
 	player2.global_transform = player2pos.global_transform
 	Globals.respawn2 = player2pos.global_transform
-	print("adding player 1 at" , player1pos.global_transform)
-	add_child(player1)
 	print("adding player 2 at" , player2pos.global_transform)
 	add_child(player2)
 
+func _player_disconnected(id):
+	print("player ", id, " has disconnected")
+	get_node(str(id)).queue_free()
+	rpc_unreliable("_player_free", id)
+	pass
 
-#func _process(delta):
-#	pass
+remote func _player_free(id):
+	get_node(str(id)).queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func menu_vis():
+	$Lobby.visible = false
+	pass
--- Lobby.gd
+++ Lobby.gd
@@ -5,18 +5,16 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-
-const MAX_PLAYERS : int = 32 
-var ip_adress = "127.0.0.1"
+#
+#const MAX_PLAYERS : int = 32 
+#var ip_adress = "127.0.0.1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var ip_adress = $ip.text
-	var port = int($port.text)
 	#first Netowkr peer connected is a built in function for when a client joins a server
 	# second means to connect to self
 	#third is the function name 
-	get_tree().connect("network_peer_connected", self, "_player_connected")
+#	get_tree().connect("network_peer_connected", self, "_player_connected")
 	pass # Replace with function body.
 
 
@@ -25,25 +23,25 @@ func _ready():
 #	pass
 
 
-func _on_Button_Host_pressed():
-	var ip_adress = $ip.text
-	var port = int($port.text)
-	var peer = NetworkedMultiplayerENet.new()
-	peer.create_server(port, MAX_PLAYERS)
-	get_tree().set_network_peer(peer)
-	print("hosting")
+#func _on_Button_Host_pressed():
+#	ip_adress = $ip.text
+#	var port = int($port.text)
+#	var peer = NetworkedMultiplayerENet.new()
+#	peer.create_server(port, MAX_PLAYERS)
+#	get_tree().set_network_peer(peer)
+#	print("hosting")
+#
+#
+#func _on_Button_join_pressed():
+#	ip_adress = $ip.text
+#	var port = int($port.text)
+#	var peer = NetworkedMultiplayerENet.new()
+#	peer.create_client(ip_adress, port)
+#	get_tree().set_network_peer(peer)
 
-
-func _on_Button_join_pressed():
-	var ip_adress = $ip.text
-	var port = int($port.text)
-	var peer = NetworkedMultiplayerENet.new()
-	peer.create_client(ip_adress, port)
-	get_tree().set_network_peer(peer)
-
-func _player_connected(id):
-	print("epic")
-	Globals.player2id = id
-	var game = preload("res://Game.tscn").instance()
-	get_tree().get_root().add_child(game)
-	hide()
+#func _player_connected(id):
+#	print("epic")
+#	Globals.player2id = id
+#	var game = preload("res://Game.tscn").instance()
+#	get_tree().get_root().add_child(game)
+#
